6da2584116bb2ae772aad97d486c5d12df004804
Nischal Addepalli <f20250781@pilani.bits-pilani.ac.in>
2026-08-29 13:15:54 +0000

    lab01 task3

--- labs/lab01/task3/rca.v
+++ labs/lab01/task3/rca.v
@@ -10,6 +10,23 @@
 // TODO: instantiate four FA_Gate instances, same chaining pattern as
 // Task 2 (FA0..FA3, carry chain c1,c2,c3).
 
+module FA_Gate(
+  input  a,
+  input  b,
+  input  cin,
+  output sum,
+  output cout
+);
+  wire ps, pc1, pc2;
+
+  xor #(2,3) (ps,  a,   b);
+  and #(2,3) (pc1, a,   b);
+  xor #(2,3) (sum, cin, ps);
+  and #(2,3) (pc2, cin, ps);
+  or  #(2,3) (cout, pc1, pc2);
+
+endmodule
+
 module rca(
   input  [3:0] a,
   input  [3:0] b,
@@ -21,5 +38,8 @@ module rca(
   wire c1, c2, c3;
 
   // TODO: your four FA_Gate instances go here.
-
+  FA_Gate FA0 (a[0],b[0],cin,sum[0],c1);
+  FA_Gate FA1 (a[1],b[1],c1,sum[1],c2);
+  FA_Gate FA2 (a[2],b[2],c2,sum[2],c3);
+  FA_Gate FA3 (a[3],b[3],c3,sum[3],cout);
 endmodule
